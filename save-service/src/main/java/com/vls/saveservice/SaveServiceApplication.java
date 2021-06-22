10086ba190b449b1ad7e50f794710ba50371a0e5
nhipnty <yennhi070@gmail.com>
2021-06-22 21:28:01 +0700

    add port and discovery

--- save-service/src/main/java/com/vls/saveservice/SaveServiceApplication.java
+++ save-service/src/main/java/com/vls/saveservice/SaveServiceApplication.java
@@ -2,8 +2,10 @@ package com.vls.saveservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SaveServiceApplication {
 
     public static void main(String[] args) {
