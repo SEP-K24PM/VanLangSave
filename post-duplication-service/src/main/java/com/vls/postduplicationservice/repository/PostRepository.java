5f2cd637bed4624ffe68a98f7af0eb829137006a
nhipnty <yennhi070@gmail.com>
2021-06-27 02:20:16 +0700

    complete save into elasticsearch in duplication service

--- post-duplication-service/src/main/java/com/vls/postduplicationservice/repository/PostRepository.java
+++ post-duplication-service/src/main/java/com/vls/postduplicationservice/repository/PostRepository.java
@@ -1,13 +1,11 @@
-package com.vls.postsearchservice.repository;
+package com.vls.postduplicationservice.repository;
 
-import com.vls.postsearchservice.model.Post;
+import com.vls.postduplicationservice.model.Post;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PostRepository extends ElasticsearchRepository<Post, UUID> {
-    List<Post> findBy(String name);
 }
--- post-duplication-service/src/main/java/com/vls/postduplicationservice/service/RabbitMQService.java
+++ post-duplication-service/src/main/java/com/vls/postduplicationservice/service/RabbitMQService.java
@@ -1,13 +1,28 @@
 package com.vls.postduplicationservice.service;
 
+import com.vls.postduplicationservice.domainobject.CategoryObject;
 import com.vls.postduplicationservice.domainobject.PostObject;
+import com.vls.postduplicationservice.domainobject.ThingObject;
+import com.vls.postduplicationservice.model.Category;
+import com.vls.postduplicationservice.model.Post;
+import com.vls.postduplicationservice.model.Thing;
+import com.vls.postduplicationservice.repository.PostRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RabbitMQService implements RabbitListenerConfigurer {
+
+    private final PostRepository postRepository;
+
+    @Autowired
+    public RabbitMQService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
 
     @Override
     public void configureRabbitListeners(RabbitListenerEndpointRegistrar rabbitListenerEndpointRegistrar) {
@@ -16,6 +31,41 @@ public class RabbitMQService implements RabbitListenerConfigurer {
 
     @RabbitListener(queues = "post_queue")
     public void receivedMessage(PostObject postObject) {
-        System.out.println("received post: "+postObject);
+        saveIntoElastic(postObject);
+    }
+
+    private void saveIntoElastic(PostObject postObject) {
+        Post post = convertToPost(postObject);
+        postRepository.save(post);
+    }
+
+    private Post convertToPost(PostObject postObject) {
+        Post post = new Post(postObject.getId(),
+                postObject.getDescription(),
+                postObject.getExchange_methods(),
+                postObject.getCreated_time(),
+                convertToThing(postObject.getThing()));
+        return post;
+    }
+
+    private Thing convertToThing(ThingObject thingObject) {
+        Thing thing = new Thing(
+                thingObject.getId(),
+                thingObject.getThing_name(),
+                thingObject.getOrigin(),
+                thingObject.getPrice(),
+                thingObject.getQuantity(),
+                thingObject.getUsed_time(),
+                convertToCategory(thingObject.getCategoryObject())
+        );
+        return thing;
+    }
+
+    private Category convertToCategory(CategoryObject categoryObject) {
+        Category category = new Category(
+                categoryObject.getId(),
+                categoryObject.getCategory_name()
+        );
+        return category;
     }
 }
