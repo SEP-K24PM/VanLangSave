e756d4ee524514c7e8a9723d181b5539ef9bdfa9
nhipnty <yennhi070@gmail.com>
2021-06-30 10:30:55 +0700

    add resource

--- search-service/src/main/java/com/vls/searchservice/controller/SearchController.java
+++ search-service/src/main/java/com/vls/searchservice/controller/SearchController.java
@@ -1,20 +1,24 @@
 package com.vls.searchservice.controller;
 
 import com.vls.searchservice.model.Post;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class SearchController {
 
     private final RestTemplate restTemplate;
 
     public SearchController(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
+    }
+
+    @GetMapping(value = "/posts")
+    public String posts() {
+        return "market";
     }
 
     @RequestMapping("/posts")
