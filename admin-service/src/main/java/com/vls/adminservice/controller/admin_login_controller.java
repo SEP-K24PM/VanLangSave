d0c02a311b81266e66911da2a178d3688dcead5d
huynhbavinh <bavinh.congviec@gmail.com>
2021-07-01 14:40:26 +0700

    update view model [admin-service]

--- admin-service/src/main/java/com/vls/adminservice/controller/admin_login_controller.java
+++ admin-service/src/main/java/com/vls/adminservice/controller/admin_login_controller.java
@@ -1,20 +1,16 @@
 package com.vls.adminservice.controller;
 
-import ch.qos.logback.classic.Logger;
-import com.vls.adminservice.AdminServiceApplication;
 import com.vls.adminservice.Module.Admin_account;
 import com.vls.adminservice.repository.admin_accountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -23,22 +19,20 @@ public class admin_login_controller {
     private admin_accountRepository admin_accRepo;
 
     @RequestMapping("/login")
-    public String Checked_admin(HttpSession session) {
-        String temp = "";
-        String users_admin = "okadatoru";
-        String password = "12345";
+    public ModelAndView Checked_admin(HttpSession session, @RequestParam(name = "username") String username , @RequestParam(name = "pwd") String pwd ) {
+        String users_admin = username; //okadatoru
+        String password = pwd; //12345
 
         List<Admin_account> listAcc = admin_accRepo.checked(users_admin, password);
         if (!listAcc.isEmpty() && listAcc.size() == 1) {
             for (Admin_account acc: listAcc) {
                 Admin_account account = new Admin_account(acc);
                 session.setAttribute("id",account.getUUID());
-                temp = "id : " + account.getUUID();
             }
-            return temp;
+            return new ModelAndView("Welcome") ;
 
         } else {
-            return "fail conn";
+            return new ModelAndView("errorLogin_admin");
         }
     }
     @RequestMapping("/logout")
@@ -50,5 +44,11 @@ public class admin_login_controller {
             return "No Session to destroys" + e;
         }
     }
+    @RequestMapping("/")
+    public ModelAndView hello(){
+        //ModelAndView mav = new ModelAndView();
+        return new ModelAndView("signin_admin");
+    }
+
 
 }
---
+++ admin-service/src/main/resources/templates/dest/main.js
@@ -0,0 +1,353 @@
+let logo = document.querySelector("header .mainHeader #burgerMenuIcon");
+let openHeader = document.querySelector("header .mainHeader");
+let btnCheck = $(".button-check");
+function toggleNavigation() {
+    $("header .mainHeader").toggleClass("open");
+    $("header .mainHeader .blacklayer").toggleClass("open");
+    $("header .mainHeader .label").toggleClass("open");
+    $("header .mainHeader #sidebar .navigation").toggleClass("open");
+    $("header .mainHeader #sidebar .signin-check").toggleClass("open");
+}
+$(logo).on("click", function () {
+    toggleNavigation();
+});
+$("header .mainHeader .blacklayer").on("click", function () {
+    toggleNavigation();
+});
+
+$(document).ready(function () {
+    if ($(".home-page").length != 0) {
+    } else if ($(".market-page").length != 0) {
+        let suggestions = [];
+        let a = document.querySelectorAll(
+            ".mainProduct .mainProduct__list .mainProduct__list-item .title"
+        );
+        a.forEach((element) => {
+            suggestions.push(element.textContent);
+        });
+        const searchWrapper = document.querySelector(".search-input");
+        const inputBox = searchWrapper.querySelector("input");
+        const suggBox = searchWrapper.querySelector(".autocom-box");
+        const icon = searchWrapper.querySelector(".icon");
+        let linkTag = searchWrapper.querySelector("a");
+        let webLink;
+
+        // if user press any key and release
+        inputBox.onkeyup = (e) => {
+            let userData = e.target.value; //user enetered data
+            let emptyArray = [];
+            if (userData) {
+                emptyArray = suggestions.filter((data) => {
+                    //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+                    return data
+                        .toLocaleLowerCase()
+                        .startsWith(userData.toLocaleLowerCase());
+                });
+                emptyArray = emptyArray.map((data) => {
+                    // passing return data inside li tag
+                    return (data = "<li>" + data + "</li>");
+                });
+                searchWrapper.classList.add("active"); //show autocomplete box
+                showSuggestions(emptyArray);
+                let allList = suggBox.querySelectorAll("li");
+                for (let i = 0; i < allList.length; i++) {
+                    //adding onclick attribute in all li tag
+                    allList[i].setAttribute("onclick", "select(this)");
+                }
+            } else {
+                searchWrapper.classList.remove("active"); //hide autocomplete box
+            }
+        };
+
+        function select(element) {
+            let selectData = element.textContent;
+            inputBox.value = selectData;
+            icon.onclick = () => {
+                webLink = "https://www.google.com/search?q=" + selectData;
+                linkTag.setAttribute("href", webLink);
+                linkTag.click();
+            };
+            searchWrapper.classList.remove("active");
+        }
+
+        function showSuggestions(list) {
+            let listData;
+            if (!list.length) {
+                userValue = inputBox.value;
+                listData = "<li>" + userValue + "</li>";
+            } else {
+                listData = list.join("");
+            }
+            suggBox.innerHTML = listData;
+        }
+    } else if ($(".detailproduct-page").length != 0) {
+        // let options = document.querySelector(
+        //     ".detailproduct-page .detailProduct .container .detail__right-options .val"
+        // ).textContent;
+        // const dataDisable = "trao đổi";
+        // if (options.toLowerCase() === dataDisable) {
+        //     checkOption = true;
+        // }
+    } else if ($(".register-exchange-page").length != 0) {
+        $(btnCheck).on("click", function () {
+            $(btnCheck).removeClass("fillColor");
+            $(btnCheck).children().removeClass("scaleNormal");
+            $(this).toggleClass("fillColor");
+            $(this).children().toggleClass("scaleNormal");
+        });
+        let btnDel = $(".button.delproduct");
+        $(btnDel).on("click", function () {
+            $(btnCheck).removeClass("fillColor");
+            $(btnCheck).children().removeClass("scaleNormal");
+        });
+    } else if ($(".signin-page").length != 0) {
+        const loginForm = document.querySelector(".signin-form");
+        const showPasswordIcon =
+            loginForm && loginForm.querySelector(".show-password");
+        const inputPassword =
+            loginForm && loginForm.querySelector('input[type="password"]');
+        showPasswordIcon.addEventListener("click", function () {
+            const inputPasswordType = inputPassword.getAttribute("type");
+            inputPasswordType === "password"
+                ? inputPassword.setAttribute("type", "text")
+                : inputPassword.setAttribute("type", "password");
+        });
+    } else if ($(".userAccount-inventory-page").length != 0) {
+        /**Inventory */
+        $(btnCheck).on("click", function () {
+            $(this).toggleClass("fillColor");
+            $(this).children().toggleClass("scaleNormal");
+        });
+    } else if ($(".addProduct-page").length != 0) {
+        $(".addProduct .data .btn-add").on("click", function () {
+            $(".noti").addClass("success");
+        });
+        $(".noti .close-icon").on("click", function () {
+            $(".noti").removeClass("success");
+        });
+    } else if ($(".userProfile-page").length != 0) {
+        /**Turn a number into star rating*/
+        var starWidth = 15;
+        $.fn.stars = function () {
+            return $(this).each(function () {
+                $(this).html(
+                    $("<span />").width(
+                        Math.max(0, Math.min(5, parseFloat($(this).html()))) *
+                            starWidth
+                    )
+                );
+            });
+        };
+        $(
+            ".userProfile-page .mainProfile .mainProfile__left .mainProfile__left-wrap span.stars"
+        ).stars();
+        $(
+            ".userProfile-page .mainProfile .mainProfile__right-list-review span.stars"
+        ).stars();
+        /** SHOW MORE TEXT*/
+        if ($(".desc-box .short-desc").height() < 55) {
+            $(".desc-box .read-more").addClass("disable");
+        } else {
+            /**Showmore btn */
+            var $el, $ps, $up, totalHeight;
+
+            $(".desc-box .button").click(function () {
+                totalHeight = 0;
+
+                $el = $(this);
+                $p = $el.parent();
+                $up = $p.parent();
+                $ps = $up.find("p:not('.read-more')");
+
+                $ps.each(function () {
+                    totalHeight += $(this).outerHeight();
+                });
+
+                $up.css({
+                    // Set height to prevent instant jumpdown when max height is removed
+                    height: $up.height(),
+                    "max-height": 9999,
+                }).animate({
+                    height: totalHeight,
+                });
+
+                // fade out read-more
+                $p.fadeOut();
+
+                // prevent jump-down
+                return false;
+            });
+        }
+        $(".cmt-box .cmt-content").each(function (index, element) {
+            if ($(element).height() < 40) {
+                $(element)
+                    .closest(".cmt-box")
+                    .find(".read-more")
+                    .addClass("disable");
+            } else {
+                /**Showmore btn */
+                var $el, $ps, $up, totalHeight;
+                $(".cmt-box .button").click(function () {
+                    totalHeight = 0;
+
+                    $el = $(this);
+                    $p = $el.parent();
+                    $up = $p.parent();
+                    $ps = $up.find("p:not('.read-more')");
+
+                    $ps.each(function () {
+                        totalHeight += $(this).outerHeight();
+                    });
+
+                    $up.css({
+                        // Set height to prevent instant jumpdown when max height is removed
+                        height: $up.height(),
+                        "max-height": 9999,
+                    }).animate({
+                        height: totalHeight,
+                    });
+
+                    // fade out read-more
+                    $p.fadeOut();
+
+                    // prevent jump-down
+                    return false;
+                });
+            }
+        });
+        /** TAB ACTIVE */
+        let panel = $(".panel");
+        $(document).on("click", ".tag-list .button-primary", function (e) {
+            e.preventDefault();
+            panel.hide();
+            // $(".tag-list .button-primary").removeClass("active");
+            // panel.eq($(this).index()).addClass("active");
+            // $(this).addClass("active");
+            $(".tag-list .button-primary").removeClass("active");
+            panel.eq($(this).index()).show();
+            $(this).addClass("active");
+        });
+    } else if ($(".userAccount-history-page").length != 0) {
+        $(".comment-wrap .button-primary.comment").on("click", function (e) {
+            e.preventDefault();
+            $(".comment-wrap .comment-box").removeClass("active");
+            $(this).parent().find(".comment-box").addClass("active");
+        });
+        $(".comment-wrap .comment-box .button-primary.sendComment").on(
+            "click",
+            function (e) {
+                e.preventDefault();
+                $(this).parent().removeClass("active");
+            }
+        );
+        // $(".cmt-box .cmt-content").each(function (index, element) {
+        //     if ($(element).height() < 40) {
+        //         $(element)
+        //             .closest(".cmt-box")
+        //             .find(".read-more")
+        //             .addClass("disable");
+        //     } else {
+        //         /**Showmore btn */
+        //         var $el, $ps, $up, totalHeight;
+        //         $(".cmt-box .button").click(function () {
+        //             totalHeight = 0;
+
+        //             $el = $(this);
+        //             $p = $el.parent();
+        //             $up = $p.parent();
+        //             $ps = $up.find("p:not('.read-more')");
+
+        //             $ps.each(function () {
+        //                 totalHeight += $(this).outerHeight();
+        //             });
+
+        //             $up.css({
+        //                 // Set height to prevent instant jumpdown when max height is removed
+        //                 height: $up.height(),
+        //                 "max-height": 9999,
+        //             }).animate({
+        //                 height: totalHeight,
+        //             });
+
+        //             // fade out read-more
+        //             $p.fadeOut();
+
+        //             // prevent jump-down
+        //             return false;
+        //         });
+        //     }
+        // });
+        // $(".comment-wrap .button-primary.comment").each(function(index, element){
+        //     e.preventDefault();
+        //     $(this)
+        // })
+    }
+});
+
+let btnPaging = $(".paging-btn span");
+$(btnPaging).on("click", function () {
+    $(btnPaging).removeClass("active");
+    $(this).toggleClass("active");
+});
+
+let $detailProduct = $(
+    ".detailproduct-page .detailProduct .container .detail__left .detail__left-slider"
+);
+$detailProduct.flickity({
+    cellAlign: "left",
+    contain: true,
+    //wrapAround: true,
+    //draggable: false,
+    prevNextButtons: false,
+    pageDots: false,
+    on: {
+        change: function (index) {
+            let number = $(
+                ".detailproduct-page .detailProduct .container .detail__left .detail__left-valimg span"
+            );
+            let indexPage = index + 1;
+            number.text(indexPage.toString().padStart(1));
+        },
+    },
+});
+$(
+    ".detailproduct-page .detailProduct .container .detail__left .detail__left-button .btn.prev"
+).on("click", function () {
+    $detailProduct.flickity("previous");
+});
+
+$(
+    ".detailproduct-page .detailProduct .container .detail__left .detail__left-button .btn.next"
+).on("click", function () {
+    $detailProduct.flickity("next");
+});
+let $anotherProduct = $(
+    ".detailproduct-page .anotherProduct .container .mainProduct__list"
+);
+
+$anotherProduct.flickity({
+    cellAlign: "left",
+    contain: true,
+    //wrapAround: true,
+    //draggable: false,
+    prevNextButtons: false,
+    pageDots: false,
+});
+$(
+    ".detailproduct-page .anotherProduct .container .product__button .btn.prev"
+).on("click", function () {
+    $anotherProduct.flickity("previous");
+});
+
+$(
+    ".detailproduct-page .anotherProduct .container .product__button .btn.next"
+).on("click", function () {
+    $anotherProduct.flickity("next");
+});
+
+$(".searchWrap .search-input input").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $(".inventory .inventory__wrap-bottom table tbody tr").filter(function () {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+});
