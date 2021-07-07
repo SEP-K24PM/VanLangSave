1fb544cbd85bdd9f32970e7e47320d60f4373187
nhipnty <yennhi070@gmail.com>
2021-07-07 21:48:30 +0700

    account service with portal ui

--- account-service/src/main/java/com/vls/accountservice/controller/AccountController.java
+++ account-service/src/main/java/com/vls/accountservice/controller/AccountController.java
@@ -33,13 +33,15 @@ public class AccountController {
         }
     }
 
-    @PostMapping("/post")
-    public ResponseEntity<Account> createTutorial() {
+    @PostMapping("/save")
+    public ResponseEntity<Account> createTutorial(@RequestBody String email) {
         Account user = new Account();
-        user.setEmail("1unreal.187pm14034@vanlanguni.vn");
+        user.setEmail(email);
+        System.out.println(email);
         try {
             Account _user = accountRepository.save(user);
             return new ResponseEntity<>(_user, HttpStatus.CREATED);
+//            return new ResponseEntity<>(HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
