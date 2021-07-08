be3e1049f2108337ccf95a5a188bd3eccedcc39b
huynhbavinh <bavinh.congviec@gmail.com>
2021-07-08 12:39:36 +0700

    Create Branch User Porfile

--- account-service/src/main/java/com/vls/accountservice/repository/AccountRepository.java
+++ account-service/src/main/java/com/vls/accountservice/repository/AccountRepository.java
@@ -3,6 +3,7 @@ package com.vls.accountservice.repository;
 import com.vls.accountservice.module.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +11,7 @@ public interface AccountRepository extends JpaRepository<Account, String> {
 
     @Query("SELECT user FROM Account user")
     List<Account> ListAllUser();
+
+    @Query("SELECT info.email FROM Account info where info.email = :email")
+    List<Account> giveAccountInfo(@Param("email") String email);
 }
