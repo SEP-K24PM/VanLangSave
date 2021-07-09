40fd6c6e65d0e87a79e70222e76641c14f88817a
huynhbavinh <bavinh.congviec@gmail.com>
2021-07-09 17:17:56 +0700

    config trade-service

--- trade-service/src/main/java/com/vls/tradeservice/model/PostRegistration.java
+++ trade-service/src/main/java/com/vls/tradeservice/model/PostRegistration.java
@@ -1,4 +1,32 @@
 package com.vls.tradeservice.model;
 
+import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "post_registration")
 public class PostRegistration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "choosen")
+    private  boolean choosen;
+
+    @Column(name = "thing_id")
+    private UUID thing_id;
+
+    @Column(name = "user_id")
+    private UUID user_id;
+
+    @Column(name = "ipost_idd")
+    private  UUID post_id;
+
+
 }
