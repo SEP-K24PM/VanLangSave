ec3fba7ae4a5e72d34270c32f9992941eb519f7c
187pm13894 <55739510+187pm13894@users.noreply.github.com>
2021-07-10 17:07:05 +0700

    đelete $project.build.dirrectory

--- admin_report-management-service/src/main/java/com/vls/adminreportmanagementservice/AdminReportManagementServiceApplication.java
+++ admin_report-management-service/src/main/java/com/vls/adminreportmanagementservice/AdminReportManagementServiceApplication.java
@@ -3,10 +3,11 @@ package com.vls.adminreportmanagementservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class AdminReportManagementServiceApplication {
 
     public static void main(String[] args) {
