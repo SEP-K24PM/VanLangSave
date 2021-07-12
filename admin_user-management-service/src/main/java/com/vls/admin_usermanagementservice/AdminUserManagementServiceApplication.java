03d33e702feb407789681742fd088f5d5e38797d
huynhbavinh <bavinh.congviec@gmail.com>
2021-07-12 13:48:47 +0700

    admin service: admin management user, function : block, show account

--- admin_user-management-service/src/main/java/com/vls/admin_usermanagementservice/AdminUserManagementServiceApplication.java
+++ admin_user-management-service/src/main/java/com/vls/admin_usermanagementservice/AdminUserManagementServiceApplication.java
@@ -2,8 +2,10 @@ package com.vls.admin_usermanagementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AdminUserManagementServiceApplication {
 
     public static void main(String[] args) {
