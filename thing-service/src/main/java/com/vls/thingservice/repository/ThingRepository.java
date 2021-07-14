1150edf90ad78e271da5c6d4ce69b46d0794d0dd
nhipnty <yennhi070@gmail.com>
2021-07-14 21:53:00 +0700

    add get list thing available of a user

--- thing-service/src/main/java/com/vls/thingservice/repository/ThingRepository.java
+++ thing-service/src/main/java/com/vls/thingservice/repository/ThingRepository.java
@@ -2,6 +2,7 @@ package com.vls.thingservice.repository;
 
 import com.vls.thingservice.model.Thing;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.UUID;
 @Repository
 public interface ThingRepository extends JpaRepository<Thing, UUID> {
     public List<Thing> findByUserid(UUID uuid);
+
+    @Query("SELECT t from Thing t left outer join Post p on p.thing_id = t.id where p.thing_id is null and t.userid = :thingId")
+    public List<Thing> findAvailableThing(UUID thingId);
 }
